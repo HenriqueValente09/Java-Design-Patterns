84c162f5d6ac0bcb7f6c655288aa75a357d4cacc
HenriqueValente09 <valentedev09@gmail.com>
2023-09-12 16:49:32 -0300

    feat: extra discounts

--- src/br/com/alura/store/budget/Budget.java
+++ src/br/com/alura/store/budget/Budget.java
@@ -5,10 +5,22 @@ import java.math.BigDecimal;
 public class Budget {
     private BigDecimal value;
     private int itemsAmount;
+    private String status;
 
     public Budget(BigDecimal value, int itemsAmount) {
         this.value = value;
         this.itemsAmount = itemsAmount;
+    }
+
+    public void applyExtraDiscount() {
+        BigDecimal extraDiscountValue = null;
+        if (status.equals("UNDER_ANALYSIS")) {
+            extraDiscountValue = new BigDecimal("0.05");
+        } else if (status.equals("APPROVED")) {
+            extraDiscountValue = new BigDecimal("0.02");
+        }
+
+        this.value = this.value.subtract(extraDiscountValue);
     }
 
     public BigDecimal getValue() {
