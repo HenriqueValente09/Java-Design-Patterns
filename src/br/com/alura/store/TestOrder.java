79375bb96df0edf864e0b02e15bac800a5c4bee2
HenriqueValente09 <valentedev09@gmail.com>
2023-09-15 21:11:02 -0300

    chore: executing actions using list of classes

--- src/br/com/alura/store/TestOrder.java
+++ src/br/com/alura/store/TestOrder.java
@@ -4,9 +4,13 @@ import br.com.alura.store.budget.Budget;
 import br.com.alura.store.order.Order;
 import br.com.alura.store.order.OrderGenerator;
 import br.com.alura.store.order.OrderGeneratorHandler;
+import br.com.alura.store.order.action.ActionAfterOrder;
+import br.com.alura.store.order.action.SaveDatabase;
+import br.com.alura.store.order.action.SendEmail;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class TestOrder {
     public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class TestOrder {
         int itemsAmount = Integer.parseInt(args[2]);
 
         OrderGenerator generator = new OrderGenerator(client, budgetValue, itemsAmount);
-        OrderGeneratorHandler handler = new OrderGeneratorHandler();
+        OrderGeneratorHandler handler = new OrderGeneratorHandler(Arrays.asList((ActionAfterOrder) new SendEmail(), (ActionAfterOrder) new SaveDatabase()));
         handler.execute(generator);
     }
 }
