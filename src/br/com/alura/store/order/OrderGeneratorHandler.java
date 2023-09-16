fe52a4cb6cbd117104b0c14f3a1a427a00dbaba2
HenriqueValente09 <valentedev09@gmail.com>
2023-09-15 21:10:07 -0300

    chore: list to execute actions

--- src/br/com/alura/store/order/OrderGeneratorHandler.java
+++ src/br/com/alura/store/order/OrderGeneratorHandler.java
@@ -1,15 +1,25 @@
 package br.com.alura.store.order;
 
 import br.com.alura.store.budget.Budget;
+import br.com.alura.store.order.action.ActionAfterOrder;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class OrderGeneratorHandler {
+
+    private List<ActionAfterOrder> actions;
+
+    public OrderGeneratorHandler(List<ActionAfterOrder> actions) {
+        this.actions = actions;
+    }
 
     public void execute (OrderGenerator data) {
         Budget budget = new Budget(data.getBudgetValue(), data.getItemsAmount());
         String client = data.getClient();
 
         Order order = new Order(client, LocalDateTime.now(), budget);
+
+        actions.forEach(a -> a.execute(order));
     }
 }
